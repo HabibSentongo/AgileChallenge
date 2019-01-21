87cf751e1b3d2d9f76b5aa59864473469428df1e
Rhytah <ritanamono@gmail.com>
2019-01-21 12:41:03 +0300

    integrate functions

--- src/app.py
+++ src/app.py
@@ -1,19 +1,22 @@
 from user_controls import UserConcerns
 from comments import Comment
 if __name__ =='__main__':
-    user=UserConcerns()
-    comment =Comment()
+    user=UserConcerns(username="",password='')
+    comment =Comment(author_id=1, message='some comment')
+    user1= {"testuser","pass"}
+    user.database=[user1]
 
     
 
     print ("Select Option")
     print(20 * "-", "MENU", 20 * "-")
     print("0.Login")
-    print("1. Write a comment")
-    print("2. Edit a comment")
-    print("3. Delete a comment")
-    print("4. Deleting all comments")
-    print("5. Logout")
+    print("1.SignUp")
+    print("2. Write a comment")
+    print("3. Edit a comment")
+    print("4. Delete a comment")
+    print("5. Deleting all comments")
+    print("6. Logout")
 
     loop = True #this keeps app running until it meets false
     task=''    
@@ -22,32 +25,34 @@ if __name__ =='__main__':
 
         if selection== 0:
             print("0.Login")
-        username=input("Enter your username:  ")
-        password=None
-        
-        while username in user.database:
+            username=input("Enter your username:  ")
+            password=''
+            
+            # while username in user.database:
             user.login_user(username,password)
             print("{} you are logged in".format(username))     
-        
-        else:
-            print("Please sign up")
-        user.create_user(username,password)
-        if selection== 1:
-            print("1.Write a comment")
-        elif selection ==2 :
-            print("2.Edit a comment")
+            
+        elif selection == 1:
+                print("1.Please sign up")
+                user.create_user(username,password)
+        elif selection== 2:
+            print("2.Write a comment")
+            comment.to_dict()
+            comment.save_comment()
+        elif selection ==3 :
+            print("3.Edit a comment")
 
             comment_id=str(input("Enter comment_id you want to edit: "))
             comment.edit_comment(comment_id)
-        elif selection ==3:
-            print("3. Delete a comment")
+        elif selection ==4:
+            print("4. Delete a comment")
             comment=str(input("Enter comment_id you want to delete: "))
          
-        elif selection==4:
-            print("3.Deleting all comments")
+        elif selection==5:
+            print("5.Deleting all comments")
 
-        elif selection ==5:
-            print("5. You are leaving application")
+        elif selection ==6:
+            print("6. You are leaving application")
             loop=False #this will set the loop to close, hence end program
         else:
             print("Selection doesnot exist. Enter a valid selection ")  #when one enters a number out of the menu range
